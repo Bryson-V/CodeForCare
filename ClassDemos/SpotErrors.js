5311ada7c607da47044d9d13ef3d6af9169fc8fe
Jaden <Leejjaden@gmail.com>
2026-07-22 15:43:48 +1000

    feat(ClassDemos): Created spot the errors for js and html

---
+++ ClassDemos/SpotErrors.js
@@ -0,0 +1,18 @@
+function calculateDiscount(userAge, cartTotal) {
+  let discount = 0;
+
+  // Rule 1: Seniors (65+) get $20 off
+  // Students (under 18) get $10 off
+
+  if (userAge = 65) {
+    discount = 20;
+  } else if (userAge < 18) {
+    discount = 10;
+  }
+
+  const finalPrice = cartTotal - discount;
+  return "Your total is: $" + finalprice;
+}
+
+let myTotal = calculateDiscount(70, 100);
+console.log(myTotal);
